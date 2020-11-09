248e90418e5d4872578ae9512b6a968990d1d387
Gregory McIntyre <greg@gregorymcintyre.com>
2020-11-10 10:00:15 +1100

    Improve ASDF detection if it's sourced in .zshrc not earlier in .zprofile

--- modules/ruby/init.zsh
+++ modules/ruby/init.zsh
@@ -6,8 +6,9 @@
 #
 
 # Load RVM into the shell session.
-if [[ ! -z "$ASDF_DIR" && -d "$ASDF_DIR/plugins/ruby" ]]; then
+if [[ ! -z "$ASDF_DIR" && -d "$ASDF_DIR/plugins/ruby" || -d "$HOME/.asdf/plugins/ruby" ]]; then
   # nothing necessary for asdf <3
+
 elif [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
   # Unset AUTO_NAME_DIRS since auto adding variable-stored paths to ~ list
   # conflicts with RVM.
@@ -16,17 +17,17 @@ elif [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
   # Source RVM.
   source "$HOME/.rvm/scripts/rvm"
 
-# Load manually installed rbenv into the shell session.
 elif [[ -s "$HOME/.rbenv/bin/rbenv" ]]; then
+  # Load manually installed rbenv into the shell session.
   path=("$HOME/.rbenv/bin" $path)
   eval "$(rbenv init - --no-rehash zsh)"
 
-# Load package manager installed rbenv into the shell session.
 elif (( $+commands[rbenv] )); then
+  # Load package manager installed rbenv into the shell session.
   eval "$(rbenv init - --no-rehash zsh)"
 
-# Load package manager installed chruby into the shell session.
 elif (( $+commands[chruby-exec] )); then
+  # Load package manager installed chruby into the shell session.
   source "${commands[chruby-exec]:h:h}/share/chruby/chruby.sh"
   if zstyle -t ':prezto:module:ruby:chruby' auto-switch; then
     source "${commands[chruby-exec]:h:h}/share/chruby/auto.sh"
@@ -35,8 +36,8 @@ elif (( $+commands[chruby-exec] )); then
     chruby_auto
   fi
 
-# Prepend local gems bin directories to PATH.
 else
+  # Prepend local gems bin directories to PATH.
   path=($HOME/.gem/ruby/*/bin(N) $path)
 fi
 
